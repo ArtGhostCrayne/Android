bc7deb739d28abd622b4e75ef2221e184995491f
ArtGhost <artghostcrayne@mail.ru>
2022-11-13 19:47:57 +0300

    ДЗ CRUD

--- app/src/main/java/ru/nmedia/MainActivity.kt
+++ app/src/main/java/ru/nmedia/MainActivity.kt
@@ -1,13 +1,17 @@
 package ru.nmedia
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nmedia.R
 import com.example.nmedia.databinding.ActivityMainBinding
-import com.example.nmedia.databinding.PostLayoutBinding
+import ru.nmedia.adapter.OnInteractionListener
 import ru.nmedia.adapter.PostsAdapter
+import ru.nmedia.dto.Post
 import ru.nmedia.repository.PostViewModel
+import ru.nmedia.util.AndroidUtils
 import java.text.DecimalFormat
 
 
@@ -31,28 +35,92 @@ fun countToString(count: Int): String {
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-//    lateinit var PostLayoutBinding: ActivityMainBinding
+    val interactionListener = object : OnInteractionListener {
+        override fun onLike(post: Post) {
+            viewModel.likeById(post.id)
+        }
 
+        override fun onRepost(post: Post) {
+            viewModel.repostById(post.id)
+        }
 
+        override fun onRemove(post: Post) {
+            viewModel.removeById(post.id)
+        }
+
+        override fun onEdit(post: Post) {
+            viewModel.editPost(post)
+        }
+    }
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    //    lateinit var PostLayoutBinding: ActivityMainBinding
     private val viewModel: PostViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         subscribe()
+
+        binding.editCancelIb.setOnClickListener {
+            binding.layoutEditInfoCl.visibility = View.GONE
+            viewModel.editCancel()
+            with(binding.textInputEt) {
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+            }
+
+        }
+
+        binding.postApplyIb.setOnClickListener {
+            with(binding.textInputEt) {
+                if (text.isNullOrBlank()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        R.string.msg_empty_post_text,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    if (viewModel.edited.value?.id == 0L) {
+                        viewModel.add(text.toString())
+                    }
+                    else{
+                        viewModel.edit( viewModel.edited.value!!.copy(content = text.toString()))
+                    }
+                    setText("")
+                    clearFocus()
+                    AndroidUtils.hideKeyboard(this)
+                }
+            }
+        }
 
     }
 
     private fun subscribe() {
-        val adapter = PostsAdapter(
-            {viewModel.likeById(it.id)},
-            {viewModel.repostById(it.id)}
-        )
+        val adapter = PostsAdapter(interactionListener)
         binding.list.adapter = adapter
-        viewModel.data.observe(this){
-            posts -> adapter.submitList(posts)
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
+
+        }
+
+        viewModel.edited.observe(this){
+            post ->
+            if (post.id !=0L){
+                with (binding.textInputEt){
+                    requestFocus()
+                    setText(post.content)
+                }
+                binding.layoutEditInfoCl.visibility = View.VISIBLE
+                binding.editedPostTitleTv.text = post.title
+                AndroidUtils.showKeyboard(binding.textInputEt)
+            }else binding.layoutEditInfoCl.visibility = View.GONE
+
+
         }
     }
 }
--- app/src/main/java/ru/nmedia/repository/PostViewModel.kt
+++ app/src/main/java/ru/nmedia/repository/PostViewModel.kt
@@ -1,10 +1,35 @@
 package ru.nmedia.repository
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.nmedia.dto.Post
 
-class PostViewModel: ViewModel() {
-    private  val repository: PostRepository = PostRepositoryInMemoryImpl()
+private val empty = Post(
+    id = 0,
+    title = "",
+    date = "",
+    content = ""
+)
+
+class PostViewModel : ViewModel() {
+    private val repository: PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.getAll()
+    val edited = MutableLiveData(empty)
     fun likeById(id: Long) = repository.likeById(id)
     fun repostById(id: Long) = repository.repostById(id)
+    fun removeById(id: Long) = repository.removeById(id)
+    fun edit(post: Post) {
+        repository.edit(post)
+        edited.value = empty
+    }
+
+    fun editCancel() {
+        edited.value = empty
+    }
+
+    fun add(text: String) = repository.add(text)
+
+    fun editPost(post: Post) {
+        edited.value = post
+    }
 }
